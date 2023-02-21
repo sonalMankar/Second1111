3eb467bdcf9957c9eef7fe104d682dae563e811a
sonal <sonal.mankar16@gmail.com>
2023-02-21 08:42:05 +0530

    adding changes in HelloWorld

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,7 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.print("Hello word through cmd");
+		System.out.print("Hello word through cmd Good Morning");
 	}
 
 }
